39ac1e0b3f0cf22624d51094b4413a78d38435ac
Neriman SEVEN <Neriman.seven123@gmail.com>
2018-07-02 10:57:22 +0300

    City,Company,Country service

---
+++ HrPortalV2.Service/CityService.cs
@@ -0,0 +1,51 @@
+﻿using HrPortalV2.Data;
+using HrPortalV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HrPortalV2.Service
+{
+   public class CityService: ICityService
+    {
+        private readonly IRepository<City> cityRepository;
+        public CityService(IRepository<City> cityRepository)
+        {
+            this.cityRepository = cityRepository;
+        }
+
+        public void Delete(string id)
+        {
+            var entity = cityRepository.Get(id);
+            cityRepository.Delete(entity);
+        }
+
+        public City Get(string id)
+        {
+            return cityRepository.Get(id, "Country");
+        }
+
+        public IEnumerable<City> GetAll()
+        {
+            return cityRepository.GetAll("Country");
+        }
+
+        public void Insert(City entity)
+        {
+            cityRepository.Insert(entity);
+        }
+
+        public void Update(City entity)
+        {
+            cityRepository.Update(entity);
+        }
+    }
+    public interface ICityService
+    {
+        IEnumerable<City> GetAll();
+        City Get(string id);
+        void Insert(City entity);
+        void Update(City entity);
+        void Delete(string id);
+    }
+}
---
+++ HrPortalV2.Service/CountryService.cs
@@ -0,0 +1,51 @@
+﻿using HrPortalV2.Data;
+using HrPortalV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HrPortalV2.Service
+{
+  public class CountryService:ICountryService
+    {
+        private readonly IRepository<Country> countryRepository;
+        public CountryService(IRepository<Country> countryRepository)
+        {
+            this.countryRepository = countryRepository;
+        }
+
+        public void Delete(string id)
+        {
+            var entity = countryRepository.Get(id);
+            countryRepository.Delete(entity);
+        }
+
+        public Country Get(string id)
+        {
+            return countryRepository.Get(id, "Country");
+        }
+
+        public IEnumerable<Country> GetAll()
+        {
+            return countryRepository.GetAll("Country");
+        }
+
+        public void Insert(Country entity)
+        {
+            countryRepository.Insert(entity);
+        }
+
+        public void Update(Country entity)
+        {
+            countryRepository.Update(entity);
+        }
+    }
+    public interface ICountryService
+    {
+        IEnumerable<Country> GetAll();
+        Country Get(string id);
+        void Insert(Country entity);
+        void Update(Country entity);
+        void Delete(string id);
+    }
+}
