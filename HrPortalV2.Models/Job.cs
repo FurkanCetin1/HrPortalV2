32439a6b5140fe97e3af31a3bbc715608954ee15
Kerem Genisel <keremgenisel@gmail.com>
2018-07-03 10:40:15 +0300

    models displayname added

--- HrPortalV2.Models/Job.cs
+++ HrPortalV2.Models/Job.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -7,15 +8,23 @@ namespace HrPortalV2.Models
 {
     public class Job:BaseEntity
     {
+        [Display(Name ="Başlık")]
         public string Title { get; set; }
+        [Display(Name = "Açıklama")]
         public string Description { get; set; }
+        [Display(Name = "Firma")]
         public string CompanyId { get; set; }
         [ForeignKey("CompanyId")]
+        [Display(Name = "Firma")]
         public Company Company { get; set; }
+        [Display(Name = "Yayın Tarihi")]
         public DateTime PublishDate { get; set; }
+        [Display(Name = "Bitiş Tarihi")]
         public DateTime EndDate { get; set; }
         public virtual ICollection<JobApplication> JobApplications { get; set; }
+        [Display(Name = "Yayınlandı mı?")]
         public bool IsApproved { get; set; }
+        [Display(Name = "Aktiv mi ?")]
         public bool IsActive { get; set; }
     }
 }
--- HrPortalV2.Models/JobApplication.cs
+++ HrPortalV2.Models/JobApplication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -7,13 +8,19 @@ namespace HrPortalV2.Models
 {
     public class JobApplication:BaseEntity
     {
+        [Display(Name = "Özgeçmiş")]
         public string ResumeId { get; set; }
         [ForeignKey("ResumeId")]
+        [Display(Name = "Özgeçmiş")]
         public Resume Resume { get; set; }
+        [Display(Name = "İş ilanı")]
         public string JobId { get; set; }
         [ForeignKey("JobId")]
+        [Display(Name = "İş ilanı")]
         public Job Job { get; set; }
+        [Display(Name = "Başlık")]
         public string Title { get; set; }
+        [Display(Name = "Açıklama")]
         public string Description { get; set; }
 
     }
