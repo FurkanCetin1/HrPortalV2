1e970a2d0842dea122f127a8848fde19caffe274
Kerem Genisel <keremgenisel@gmail.com>
2018-07-03 10:14:44 +0300

    jobs-controller and jobs-index created

---
+++ HrPortalV2.Web/Controllers/JobsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HrPortalV2.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HrPortalV2.Web.Controllers
+{
+    public class JobsController : Controller
+    {
+        private readonly IJobService jobService;
+
+        public JobsController(IJobService jobService)
+        {
+            this.jobService = jobService;
+        }
+        public IActionResult Index()
+        {
+            var jobs = jobService.GetAll(); 
+            return View(jobs);
+        }
+    }
+}
