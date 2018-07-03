ee16037c225afaca86a12a45ac60b0ebc93ecb92
Kerem Genisel <keremgenisel@gmail.com>
2018-07-03 11:59:43 +0300

    job create

--- HrPortalV2.Web/Controllers/JobsController.cs
+++ HrPortalV2.Web/Controllers/JobsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HrPortalV2.Models;
 using HrPortalV2.Service;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,50 @@ namespace HrPortalV2.Web.Controllers
         {
             var jobs = jobService.GetAll(); 
             return View(jobs);
+        }
+
+        //Create
+        public ActionResult Create()
+        {
+            var job = new Job();
+            return View(job);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                jobService.Insert(job);
+                return RedirectToAction(nameof(Edit), new { id = job.Id, saved= true });
+            }
+            else
+            {
+                return View(job);
+            }
+        }
+
+        //Edit
+        public ActionResult Edit(string id, bool saved)
+        {
+            var job = jobService.Get(id);
+            return View(job);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                jobService.Update(job);
+                return RedirectToAction(nameof(Edit), new { id = job.Id });
+            }
+            else
+            {
+                return View(job);
+            }
         }
     }
 }
