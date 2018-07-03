23cc06bccf221ce46917ecf0ed072a6f484fcfef
Neriman SEVEN <Neriman.seven123@gmail.com>
2018-07-03 10:14:22 +0300

    Companies

---
+++ HrPortalV2.Web/Controllers/CompaniesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HrPortalV2.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HrPortalV2.Web.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        private readonly ICompanyService companyService;
+        public CompaniesController(ICompanyService companyService)
+        {
+            this.companyService = companyService;
+        }
+        public IActionResult Index()
+        {
+            var companys = companyService.GetAll();
+            return View(companys);
+        }
+    }
+}
