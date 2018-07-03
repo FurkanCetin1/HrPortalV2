f9c430e22469a2bdc7e42f572990d783fea6f6cf
Neriman SEVEN <Neriman.seven123@gmail.com>
2018-07-03 10:41:07 +0300

    Display(Name)

--- HrPortalV2.Models/Company.cs
+++ HrPortalV2.Models/Company.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -7,22 +8,35 @@ namespace HrPortalV2.Models
 {
     public class Company:BaseEntity
     {
+        [Display(Name = "Ad")]
         public string Name { get; set; }
+        [Display(Name = "Açıklama")]
         public string Description { get; set; }
+        [Display(Name = "Ülke")]
         public string CountryId { get; set; }
         [ForeignKey("CountryId")]
+        [Display(Name = "Ülke")]
         public Country Country { get; set; }
+        [Display(Name = "Şehir")]
         public string CityId { get; set; }
         [ForeignKey("CityId")]
+        [Display(Name = "Şehir")]
         public City City {get; set;}
+        [Display(Name = "İlçe")]
         public string CountyId { get; set; }
         [ForeignKey("CountyId")]
+        [Display(Name = "İlçe")]
         public County County { get; set; }
+        [Display(Name = "Adres")]
         public string Address { get; set; }
+        [Display(Name = "Sektor")]
         public string SectorId { get; set; }
         [ForeignKey("SectorId")]
+        [Display(Name = "Sektor")]
         public Sector Sector { get; set; }
+        [Display(Name = "Onaylandı mı?")]
         public bool IsApproved { get; set; }
+        [Display(Name = "Aktiv mi?")]
         public bool IsActive { get; set; }
     }
 }
--- HrPortalV2.Models/Country.cs
+++ HrPortalV2.Models/Country.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HrPortalV2.Models
 {
     public class Country:BaseEntity
     {
+        [Display(Name = "Ad")]
         public string Name { get; set; }
     }
 }
