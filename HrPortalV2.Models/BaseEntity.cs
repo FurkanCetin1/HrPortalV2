6c2ea41b82399a5e40429ed85ce11316376c497c
sezer önalan <sezer.onalan@gmail.com>
2018-07-03 10:50:55 +0300

    MilitaryStatus Base Entity

--- HrPortalV2.Models/BaseEntity.cs
+++ HrPortalV2.Models/BaseEntity.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HrPortalV2.Models
 {
     public class BaseEntity
     {
         public string Id { get; set; }
+        [Display(Name = "Oluşturma Tarihi")]
         public DateTime CreateDate { get; set; }
+        [Display(Name = "Oluşturan Kullanıcı")]
         public string CreatedBy { get; set; }
+        [Display(Name = "Güncelleme Tarihi")]
         public DateTime UpdateDate { get; set; }
+        [Display(Name = "Oluşturan Kullanıcı")]
         public string UpdatedBy { get; set; }
+        [Display(Name = "Ip Adresi")]
         public string IPAddress { get; set; }
     }
 }
