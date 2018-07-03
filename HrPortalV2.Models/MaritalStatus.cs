3c316cb61124de4c0e904cc6ef9f2cde65af4960
Kerem Genisel <keremgenisel@gmail.com>
2018-07-03 10:44:22 +0300

    marital statur displayname added

--- HrPortalV2.Models/MaritalStatus.cs
+++ HrPortalV2.Models/MaritalStatus.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HrPortalV2.Models
 {
     public enum MaritalStatus
     {
+        [Display(Name = "Bekar")]
         Single = 1,
+        [Display(Name = "Evli")]
         Married = 2
     }
 }
