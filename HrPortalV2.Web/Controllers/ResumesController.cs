ce80173cdfb17edb28ac4cc49b4b5ca37569ac73
sezer önalan <sezer.onalan@gmail.com>
2018-07-04 10:24:49 +0300

    resume create edit

--- HrPortalV2.Web/Controllers/ResumesController.cs
+++ HrPortalV2.Web/Controllers/ResumesController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using HrPortalV2.Models;
 using HrPortalV2.Service;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -13,8 +16,10 @@ namespace HrPortalV2.Web.Controllers
     {
         private readonly IResumeService resumeService;
         private readonly IOccupationService occupationService;
-        public ResumesController(IResumeService resumeService,IOccupationService occupationService)
+        private readonly IHostingEnvironment _environment;
+        public ResumesController(IResumeService resumeService,IOccupationService occupationService,IHostingEnvironment _environment)
         {
+            this._environment = _environment;
             this.resumeService = resumeService;
             this.occupationService = occupationService;
         }
@@ -30,18 +35,63 @@ namespace HrPortalV2.Web.Controllers
             return View(resume);
         }
         [HttpPost]
-        public IActionResult Create(Resume resume)
+        public async Task<IActionResult> Create(Resume resume, IFormFile File)
         {
             if (ModelState.IsValid)
             {
+                if (File != null && File.Length > 0)
+                {
+                    //upload işlemi yapmak için konum belirle
+                    string path = Path.Combine(_environment.WebRootPath, "Uploads", File.FileName);
+                    //uploads dizini yoksa oluştur
+                    if (!Directory.Exists(Path.Combine(_environment.WebRootPath, "Uploads")))
+                    {
+                        Directory.CreateDirectory(Path.Combine(_environment.WebRootPath, "Uploads"));
+                    }
+                    //belirlenen konuma upload yapılır.
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await File.CopyToAsync(stream);
+                    }
+                    resume.Photo = File.FileName;
+                }
                 resumeService.Insert(resume);
                 return RedirectToAction(nameof(Edit), new { id = resume.Id, saved = true });
             }
             ViewData["OccupationId"] = new SelectList(occupationService.GetAll(), "Id", "Name");
             return View(resume);
         }
-        public IActionResult Edit()
+        public IActionResult Edit(string id, bool saved)
         {
+            var resume = resumeService.Get(id);
+            ViewData["OccupationId"] = new SelectList(occupationService.GetAll(), "Id", "Name",resume.OccupationId);
+            return View(resume);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Resume resume, IFormFile File)
+        {
+            if (ModelState.IsValid)
+            {
+                if (File != null && File.Length > 0)
+                {
+                    //upload işlemi yapmak için konum belirle
+                    string path = Path.Combine(_environment.WebRootPath, "Uploads", File.FileName);
+                    //uploads dizini yoksa oluştur
+                    if (!Directory.Exists(Path.Combine(_environment.WebRootPath, "Uploads")))
+                    {
+                        Directory.CreateDirectory(Path.Combine(_environment.WebRootPath, "Uploads"));
+                    }
+                    //belirlenen konuma upload yapılır.
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await File.CopyToAsync(stream);
+                    }
+                    resume.Photo = File.FileName;
+                }
+                resumeService.Update(resume);
+                return RedirectToAction(nameof(Edit), new { id = resume.Id, saved = true });
+            }
+            ViewData["OccupationId"] = new SelectList(occupationService.GetAll(), "Id", "Name",resume.OccupationId);
             return View();
         }
     }
