9d10bfa3183c2e3a5767e0906b68a3c5dc6901a0
Murat Demirci <mdemirci@outlook.com>
2018-08-09 16:54:16 +0300

    access denied

--- HrPortalV2.Web/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ HrPortalV2.Web/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -55,8 +55,8 @@ namespace HrPortalV2.Web.Areas.Identity.Pages.Account
 
                 await _emailSender.SendEmailAsync(
                     Input.Email,
-                    "Reset Password",
-                    $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    "Parolayı Sıfırla",
+                    $"<a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>buraya tıklayarak</a> parolanızı sıfırlayabilirsiniz.");
 
                 return RedirectToPage("./ForgotPasswordConfirmation");
             }
